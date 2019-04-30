3b751d8031afb781a7ce4733a84b9fcdb34ffbb7
Camilo <camilo.guio@trycore.com.co>
2019-04-30 14:37:39 -0500

    Primer Commit

---
+++ src/Componentes/Header/buscador.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Buscador extends Component {
+    render() {
+
+        return (
+
+            <div>
+                <form>
+                   <input type="text"></input>
+                   <button>Buscar</button> 
+                </form>
+            </div>
+
+        )
+
+    }
+}
+
+export default Buscador;
---
+++ src/Componentes/Header/header.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import Buscador from './buscador';
+import Style from './Css/styleglobal.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+            <div className={Style.topBar}> Bienvenido al visor de Super Heroes </div>
+            <Buscador/>
+            </div>
+        )
+    }
+}
+
+export default Header;
